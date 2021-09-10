f4116dde9cf37e7d4a3546d1d40e9e83b7ca8463
s-li1 <stevenli8892@hotmail.com.au>
2021-09-11 09:12:18 +1000

    added user service inteface

---
+++ src/SATEvening.BLL/Services/IUsersService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using SATEvening.BLL.Models;
+
+namespace SATEvening.BLL.Services
+{
+    public interface IUsersService
+    {
+        IEnumerable<User> GetAll();
+        User GetByEmail(string email);
+        Task<SignInResult> Authenticate(User user, string password);
+        Task<IdentityResult> Create(User user, string password);
+    }
+}
