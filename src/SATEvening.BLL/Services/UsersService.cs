b05b8f220f5a8bcfde44c5b5abc6242db1f7ab46
s-li1 <stevenli8892@hotmail.com.au>
2021-09-11 12:05:34 +1000

    added user service tests

---
+++ src/SATEvening.BLL/Services/UsersService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using SATEvening.BLL.Models;
+
+namespace SATEvening.BLL.Services
+{
+    public class UsersService : IUsersService
+    {
+        public UsersService()
+        {
+        }
+
+        public Task<SignInResult> Authenticate(User user, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityResult> Create(User user, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityResult> Delete(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityResult> Update(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
