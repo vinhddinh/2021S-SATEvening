bc10b7c5b4f292931f0700c27ad719d504c641a1
s-li1 <stevenli8892@hotmail.com.au>
2021-09-13 20:41:40 +1000

    added basic controller tests

---
+++ test/SATEvening.Web.Tests/AccountControllerTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SATEvening.BLL.Models;
+using SATEvening.DAL.Models;
+using SATEvening.Web.Controllers;
+using SATEvening.Web.Models;
+using Xunit;
+
+namespace SATEvening.Web.Tests
+{
+    public class AccountControllerTests
+    {
+        public AccountControllerTests()
+        {
+
+        }
+
+        [Fact]
+        public async Task ValidRegistrationDetailsShouldReturnOkResponse()
+        {
+            var user = new UserModel { Email = "test@uts.edu.au", UserName = "test123", FirstName = "test", LastName = "me", Password = "1@testL" };
+
+            var mockUserStore = new Mock<IUserStore<AppUser>>();
+            var mockUserManager = new Mock<UserManager<AppUser>>(mockUserStore.Object, null, null, null, null, null, null, null, null);
+
+            mockUserManager.Setup(m => m.CreateAsync(It.IsAny<User>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Success);
+
+            var controller = new AccountController(mockUserManager.Object);
+
+            var result = await controller.Register(user);
+
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task MissingRegistrationDetailsShouldReturnBadResponse()
+        {
+            var user = new UserModel { Email = "", UserName = "test123", FirstName = "test", LastName = "me", Password = "1@testL" };
+
+            var mockUserStore = new Mock<IUserStore<AppUser>>();
+            var mockUserManager = new Mock<UserManager<AppUser>>(mockUserStore.Object, null, null, null, null, null, null, null, null);
+
+            mockUserManager.Setup(m => m.CreateAsync(It.IsAny<User>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Failed());
+
+            var controller = new AccountController(mockUserManager.Object);
+
+            var result = await controller.Register(user);
+
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task InvalidRegistrationDetailsShouldReturnBadResponse()
+        {
+            var user = new UserModel { Email = "test@uts.edu.au", UserName = "test123", FirstName = "test", LastName = "me", Password = "thispasswordistoosimple" };
+
+            var mockUserStore = new Mock<IUserStore<AppUser>>();
+            var mockUserManager = new Mock<UserManager<AppUser>>(mockUserStore.Object, null, null, null, null, null, null, null, null);
+
+            mockUserManager.Setup(m => m.CreateAsync(It.IsAny<User>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Failed());
+
+            var controller = new AccountController(mockUserManager.Object);
+
+            var result = await controller.Register(user);
+
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+    }
+}
