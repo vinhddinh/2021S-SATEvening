3c8724384f687fa982994341bfa51fd83a5f32b6
dsouzar1 <regina.c.dsouza@student.uts.edu.au>
2021-09-21 18:20:14 +1000

    Test Header and Login-In Page

--- src/SATEvening.Web/ClientApp/src/components/NavMenu.js
+++ src/SATEvening.Web/ClientApp/src/components/NavMenu.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
+import logo from '../assets/UTSlogo3.png';
 
 export class NavMenu extends Component {
   static displayName = NavMenu.name;
@@ -24,23 +25,14 @@ export class NavMenu extends Component {
   render () {
     return (
       <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
+        <Navbar className="navbar-expand-sm navbar-toggleable-sm border-bottom header" light>
           <Container>
-            <NavbarBrand tag={Link} to="/">SATEvening</NavbarBrand>
-            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-            <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-              <ul className="navbar-nav flex-grow">
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
-                </NavItem>
-              </ul>
-            </Collapse>
+            <NavbarBrand to="/"><img src={logo}/></NavbarBrand>
+            <ul className="navbar-nav flex-grow">
+            <NavItem>
+                <NavLink tag={Link} className="text-white" to="/">MySchedulingAssistant</NavLink>
+            </NavItem>
+            </ul>
           </Container>
         </Navbar>
       </header>
