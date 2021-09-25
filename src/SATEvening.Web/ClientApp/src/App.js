70cc8a5fa33571bd2830bac7438c3500dcc04913
s-li1 <stevenli8892@hotmail.com.au>
2021-09-25 11:38:01 +1000

    removed counter componenet

--- src/SATEvening.Web/ClientApp/src/App.js
+++ src/SATEvening.Web/ClientApp/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { Counter } from './components/Counter';
 import { RegistrationForm } from './components/RegistrationForm';
 import './custom.css'
 
@@ -14,8 +13,7 @@ export default class App extends Component {
     return (
       <Layout>
         <Route exact path='/' component={Home} />
-            <Route path='/counter' component={Counter} />
-            <Route path='/register' component={RegistrationForm} />
+        <Route path='/register' component={RegistrationForm} />
       </Layout>
     );
   }
