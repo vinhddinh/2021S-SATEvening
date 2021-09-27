60d47f911fdf97cc1565c658dbf196204057fe67
s-li1 <stevenli8892@hotmail.com.au>
2021-09-27 15:30:51 +1000

    created custom exceptions for authentication actions

---
+++ src/SATEvening.BLL/Exceptions/BadRequestException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SATEvening.BLL.Exceptions
+{
+    public class BadRequestException : Exception
+    {
+        public BadRequestException(string message) : base(message)
+        {
+        }
+    }
+}
