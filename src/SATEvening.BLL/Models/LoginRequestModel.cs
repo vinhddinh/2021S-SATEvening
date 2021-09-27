621a2221a0e3c95a7c730f619eb0c5be4dfb6deb
s-li1 <stevenli8892@hotmail.com.au>
2021-09-27 15:36:18 +1000

    new request response models in service layer

---
+++ src/SATEvening.BLL/Models/LoginRequestModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SATEvening.BLL.Models
+{
+    public class LoginRequestModel
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ src/SATEvening.BLL/Models/UserResponseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SATEvening.BLL.Models
+{
+    public class UserResponseModel
+    {
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+    }
+}
