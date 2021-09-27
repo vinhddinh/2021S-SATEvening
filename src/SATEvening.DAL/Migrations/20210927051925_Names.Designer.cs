7dc65286789295d1797357fd47d7b9038a5f8dbf
s-li1 <stevenli8892@hotmail.com.au>
2021-09-27 15:42:24 +1000

    update migrations

--- src/SATEvening.DAL/Migrations/20210927051925_Names.Designer.cs
+++ src/SATEvening.DAL/Migrations/20210927051925_Names.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SATEvening.DAL.Contexts;
 
 namespace SATEvening.DAL.Migrations
 {
     [DbContext(typeof(IdentityDataContext))]
-    partial class IdentityDataContextModelSnapshot : ModelSnapshot
+    [Migration("20210927051925_Names")]
+    partial class Names
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
