a105012bf77bcdc9b48731bccfce2fbcd729521b
s-li1 <stevenli8892@hotmail.com.au>
2021-09-27 10:38:39 +1000

    auth service contains auth logic

---
+++ src/SATEvening.BLL/Services/AuthService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using SATEvening.BLL.Models;
+using SATEvening.BLL.Services.Interfaces;
+using SATEvening.DAL.Models;
+
+namespace SATEvening.BLL.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AuthService(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public async Task<string> LoginAsync(User user, string password)
+        {
+            if (UserExists())
+        }
+
+        public async Task<string> RegisterAsync(User user, string password)
+        {
+            var isExistingUser = await _userManager.FindByNameAsync(user.UserName) != null;
+
+            if (isExistingUser)
+            {
+                return string.Format("The username {0} already exists.", user.UserName);
+            }
+
+            var result = await _userManager.CreateAsync(user, password);
+
+            if (!result.Succeeded)
+            {
+                return string.Format("User successfully registered with username {0}", user.UserName);
+            }
+
+            return string.Format("Registration Failed with the following errors {0}", result.Errors);
+        }
+    }
+}
---
+++ src/SATEvening.BLL/Services/Interfaces/IAuthService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using SATEvening.BLL.Models;
+
+namespace SATEvening.BLL.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<string> RegisterAsync(User user, string password);
+        Task<string> LoginAsync(User user, string password);
+    }
+}
