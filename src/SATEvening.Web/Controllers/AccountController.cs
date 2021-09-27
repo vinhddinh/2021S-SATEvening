3b0de3e05f9933d63da20899cd40a3e0bc745fd5
s-li1 <stevenli8892@hotmail.com.au>
2021-09-27 15:40:34 +1000

    update controller to use auth service

--- src/SATEvening.Web/Controllers/AccountController.cs
+++ src/SATEvening.Web/Controllers/AccountController.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using SATEvening.BLL.Exceptions;
 using SATEvening.BLL.Models;
+using SATEvening.BLL.Services.Interfaces;
 using SATEvening.DAL.Models;
 using SATEvening.Web.Models;
 
@@ -13,60 +15,58 @@ namespace SATEvening.Web.Controllers
     [Route("api/[controller]")]
     public class AccountController : Controller
     {
-        private readonly UserManager<AppUser> _userManager;
-        private readonly SignInManager<AppUser> _signInManager;
+        private readonly IAuthService _authService;
 
-        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        public AccountController(IAuthService authService)
         {
-            _userManager = userManager;
-            _signInManager = signInManager;
+            _authService = authService;
         }
 
         [HttpPost]
         [Route("register")]
-        public async Task<IActionResult> Register([FromBody] UserModel model)
+        public async Task<IActionResult> Register([FromBody] UserRequestModel model)
         {
-            if (ModelState.IsValid || model != null)
+
+            if (!ModelState.IsValid || model == null)
             {
-                var user = new User { Email = model.Email, UserName = model.UserName, FirstName = model.UserName, LastName = model.LastName };
-
-                var result = await _userManager.CreateAsync(user, model.Password);
-
-                if (result.Succeeded)
-                {
-                    return Ok(new { Message = "User Registration was Successful" });
-                }
-
-                return new BadRequestObjectResult(new { Message = "Registration Failed", Errors = result.Errors });
+                return new BadRequestObjectResult(new { Message = "Registration Failed: Invalid Input" });
             }
 
-            return new BadRequestObjectResult(new { Message = "Registration Failed: Invalid Input" });
+            try
+            {
+                return Ok(await _authService.RegisterAsync(model));
+            }
+            catch (AlreadyExistsException ex)
+            {
+                return new BadRequestObjectResult(ex.Message);
+            }
+            catch (BadRequestException ex)
+            {
+                return new BadRequestObjectResult(ex.Message);
+            }
         }
 
         [HttpPost]
         [Route("login")]
-        public async Task<IActionResult> Login([FromBody] UserLoginModel model)
+        public async Task<IActionResult> Login([FromBody] LoginRequestModel model)
         {
             if (!ModelState.IsValid || model == null)
             {
                 return new BadRequestObjectResult(new { Message = "Login Failed: Invalid Input" });
             }
 
-            var user = await _userManager.FindByNameAsync(model.UserName);
-
-            if (user == null)
+            try
             {
-                return new BadRequestObjectResult(new { Message = "Username could not be found" });
+                return Ok(await _authService.LoginAsync(model));
             }
-
-            var result = await _signInManager.PasswordSignInAsync(user, model.Password, isPersistent: false, lockoutOnFailure: false);
-
-            if (result.Succeeded)
+            catch (NotFoundException ex)
             {
-                return Ok(new { Message = "User Registration was Successful" });
+                return new BadRequestObjectResult(ex.Message);
             }
-
-            return new BadRequestObjectResult(new { Message = "Login Failed: Incorrect Password" });
+            catch (BadRequestException ex)
+            {
+                return new BadRequestObjectResult(ex.Message);
+            }
         }
     }
 }
