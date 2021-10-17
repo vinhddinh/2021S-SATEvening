d75fb7a32fd3596965644fe799c7c6199d9f0689
s-li1 <stevenli8892@hotmail.com.au>
2021-10-17 18:01:45 +1100

    add authmessagesenderoptions

--- src/SATEvening.BLL/Settings/AuthMessageSenderOptions.cs
+++ src/SATEvening.BLL/Settings/AuthMessageSenderOptions.cs
@@ -3,8 +3,6 @@ namespace SATEvening.BLL.Settings
 {
     public class AuthMessageSenderOptions
     {
-        public AuthMessageSenderOptions()
-        {
-        }
+        public const string SendGridKey = "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"; // probably should move this elsewhere
     }
 }
