d1b935b5749fd1d02e54cedeec71fd475563a7a8
s-li1 <stevenli8892@hotmail.com.au>
2021-10-20 19:14:22 +1100

    typo fix sender email

--- src/SATEvening.BLL/Settings/AuthMessageSenderOptions.cs
+++ src/SATEvening.BLL/Settings/AuthMessageSenderOptions.cs
@@ -6,6 +6,6 @@ namespace SATEvening.BLL.Settings
         public const string SendGridKey = "SendGridKey"; // probably should move this elsewhere
         public string Key = "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";
         public string TemplateId = "d-c5b34d23bb674137bab293d6d9862016";
-        public string SenderEmail = "Josephine.A.Vu @student.uts.edu.au";
+        public string SenderEmail = "Josephine.A.Vu@student.uts.edu.au";
     }
 }
