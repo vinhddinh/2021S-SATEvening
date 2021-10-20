6e42234c65e4f4091adeef4081e183dbe75194bf
s-li1 <stevenli8892@hotmail.com.au>
2021-10-20 15:52:01 +1100

    add properties for options
    i think i will find a better way to store this information soon

--- src/SATEvening.BLL/Settings/AuthMessageSenderOptions.cs
+++ src/SATEvening.BLL/Settings/AuthMessageSenderOptions.cs
@@ -3,6 +3,9 @@ namespace SATEvening.BLL.Settings
 {
     public class AuthMessageSenderOptions
     {
-        public const string SendGridKey = "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"; // probably should move this elsewhere
+        public const string SendGridKey = "SendGridKey"; // probably should move this elsewhere
+        public string Key = "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";
+        public string TemplateId = "d-c5b34d23bb674137bab293d6d9862016";
+        public string SenderEmail = "Josephine.A.Vu @student.uts.edu.au";
     }
 }
