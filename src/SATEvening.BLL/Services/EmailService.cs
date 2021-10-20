3bbe905d512ea6510af8abdc589e10d2f1a1705e
s-li1 <stevenli8892@hotmail.com.au>
2021-10-20 15:50:50 +1100

    create email service

---
+++ src/SATEvening.BLL/Services/EmailService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using SATEvening.BLL.Services.Interfaces;
+using SATEvening.BLL.Settings;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace SATEvening.BLL.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly AuthMessageSenderOptions _senderOptions;
+
+        public EmailService(IOptions<AuthMessageSenderOptions> sendOptions)
+        {
+            _senderOptions = sendOptions.Value;
+        }
+
+        public Task SendEmailAsync(string email)
+        {
+            return Execute(email);
+        }
+
+        private Task Execute(string email)
+        {
+            var client = CreateClient();
+            var msg = CreateMessageToRecipient(email);
+
+            return client.SendEmailAsync(msg);
+        }
+
+        private SendGridClient CreateClient()
+        {
+            return new SendGridClient(_senderOptions.Key);
+        }
+
+        private SendGridMessage CreateMessageToRecipient(string email)
+        {
+            var message = new SendGridMessage()
+            {
+                From = new EmailAddress(_senderOptions.SenderEmail),
+            };
+
+            message.AddTo(new EmailAddress(email));
+            message.SetTemplateId(_senderOptions.TemplateId);
+
+            return message;
+        }
+    }
+}
---
+++ src/SATEvening.BLL/Services/Interfaces/IEmailService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SATEvening.BLL.Services.Interfaces
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(string email);
+    }
+}
