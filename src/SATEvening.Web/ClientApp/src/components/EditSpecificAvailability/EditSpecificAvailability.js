010ffeac6d61886ec2bf4bfee1c773573f92ff01
ReubenChampion <13551395@student.uts.edu.au>
2021-10-24 17:30:22 +1100

    Added the table into specific and edit specific

--- src/SATEvening.Web/ClientApp/src/components/EditSpecificAvailability/EditSpecificAvailability.js
+++ src/SATEvening.Web/ClientApp/src/components/EditSpecificAvailability/EditSpecificAvailability.js
@@ -1,47 +1,194 @@
 ﻿import React, { Component } from "react";
 //import Table from 'react-reactstrap/Table';
 import "./EditSpecificAvailability.css";
+import { Link } from 'react-router-dom';
+import TableDragSelect from "react-table-drag-select";
 
 export class EditSpecificAvailability extends Component {
     static displayName = EditSpecificAvailability.name;
 
+    initState = {
+        savedCells: [],
+        cells: [
+            [false, false],
+            [false, false],
+            [false, false],
+            [false, false],
+            [false, false],
+            [false, false],
+            [false, false],
+            [false, false],
+            [false, false],
+            [false, false],
+            [false, false],
+            [false, false],
+            [false, false],
+            [false, false],
+            [false, false],
+            [false, false],
+            [false, false],
+            [false, false],
+            [false, false],
+            [false, false],
+            [false, false],
+            [false, false],
+            [false, false],
+            [false, false],
+            [false, false],
+            [false, false],
+            [false, false]
+        ]
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            savedCells: this.initState.savedCells,
+            cells: this.initState.cells
+        };
+    }
 
     render() {
-
-        function addSpecific() { 
-
-        }
 
         return (
             <div>
                 <body>
+                    <div class="grid">
                     <header>
-                        <h1 class="title"> Edit Specific Availability </h1>
+                        <h1 id="title"> Edit Specific Availability </h1>
                     </header>
-                    <button id="addDate" onClick={addSpecific}>Save</button>
-                    //<Link to= "/edit-specific-availability
-                    <form onSubmit= {this.handleSubmit}>
+                        <Link to="/specific-availability" id="save">Save</Link>
+                    </div>
+                    <form>
                         <div>
-                        <label class ="text">
-                            <div> 
-                            Start Date
-                            </div>
-                        </label>
-                       <div>
-                                <input type= "text" name= "Start Date" >
+                            <label class="text"> Start Date </label>
                         </div>
+                        <div>
+                            <input type="text" name="Start Date" placeholder="October 25th" />
                         </div>
-                       <label class = "text">
-                           <div>
-                           End Date
-                           </div>
-                        </label>
-                           <div>
-                           <input type = "text" name = "End Date" >
-                            </div>
+                        <div>
+                            <label class="text"> End Date</label>
+                        </div >
+                        <div >
+                            <input type="text" name="End Date" placeholder="October 25th"/>
+                        </div>
+                    </form>
 
-
-                                
+                    <div id="Table2" class="myDisabledDiv">
+                        <TableDragSelect
+                            value={this.state.cells}
+                            onChange={cells => this.setState({ cells })}
+                        >
+                            <tr>
+                                <td disabled />
+                                <td disabled>Monday October 25th </td>
+                            </tr>
+                            <tr>
+                                <td disabled> 08:00 </td>
+                                <td />
+                            </tr>
+                            <tr>
+                                <td disabled />
+                                <td />
+                            </tr>
+                            <tr>
+                                <td disabled> 09:00 </td>
+                                <td />
+                            </tr>
+                            <tr>
+                                <td disabled />
+                                <td />
+                            </tr>
+                            <tr>
+                                <td disabled> 10:00 </td>
+                                <td />
+                            </tr>
+                            <tr>
+                                <td disabled />
+                                <td />
+                            </tr>
+                            <tr>
+                                <td disabled> 11:00 </td>
+                                <td />
+                            </tr>
+                            <tr>
+                                <td disabled />
+                                <td />
+                            </tr>
+                            <tr>
+                                <td disabled> 12:00 </td>
+                                <td />
+                            </tr>
+                            <tr>
+                                <td disabled />
+                                <td />
+                            </tr>
+                            <tr>
+                                <td disabled> 13:00 </td>
+                                <td />
+                            </tr>
+                            <tr>
+                                <td disabled />
+                                <td />
+                            </tr>
+                            <tr>
+                                <td disabled> 14:00 </td>
+                                <td />
+                            </tr>
+                            <tr>
+                                <td disabled />
+                                <td />
+                            </tr>
+                            <tr>
+                                <td disabled> 15:00 </td>
+                                <td />
+                            </tr>
+                            <tr>
+                                <td disabled />
+                                <td />
+                            </tr>
+                            <tr>
+                                <td disabled> 16:00 </td>
+                                <td />
+                            </tr>
+                            <tr>
+                                <td disabled />
+                                <td />
+                            </tr>
+                            <tr>
+                                <td disabled> 17:00 </td>
+                                <td />
+                            </tr>
+                            <tr>
+                                <td disabled />
+                                <td />
+                            </tr>
+                            <tr>
+                                <td disabled> 18:00 </td>
+                                <td />
+                            </tr>
+                            <tr>
+                                <td disabled />
+                                <td />
+                            </tr>
+                            <tr>
+                                <td disabled> 19:00 </td>
+                                <td />
+                            </tr>
+                            <tr>
+                                <td disabled />
+                                <td />
+                            </tr>
+                            <tr>
+                                <td disabled> 20:00 </td>
+                                <td />
+                            </tr>
+                            <tr>
+                                <td disabled />
+                                <td />
+                            </tr>
+                        </TableDragSelect>
+                    </div>
                 </body>
             </div>
          );
--- src/SATEvening.Web/ClientApp/src/components/SpecificAvailability/SpecificAvailability.js
+++ src/SATEvening.Web/ClientApp/src/components/SpecificAvailability/SpecificAvailability.js
@@ -1,7 +1,9 @@
 ﻿import React, { Component } from "react";
-//import Table from 'react-reactstrap/Table';
+import { Table } from 'reactstrap';
 import "./SpecificAvailability.css";
 import { Link } from 'react-router-dom';
+//import Table from 'react-bootstrap';
+
 
 export class SpecificAvailability extends Component {
     static displayName = SpecificAvailability.name;
@@ -24,6 +26,49 @@ export class SpecificAvailability extends Component {
                     </div>
                     <Link to="/edit-specific-availability" id="EditSpecificAvailability"> Add Date</Link>
                 </body>
+
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Start Date</th>
+                            <th>Start Time</th>
+                            <th>End Date</th>
+                            <th>End Time</th>
+                            <th>Notes</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>October 25th 2021</td>
+                            <td>00:00</td>
+                            <td>October 26th 2021</td>
+                            <td>23:59</td>
+                            <td>Not Available</td>
+                            <td><button class="edit">Edit</button></td>
+                            <td><button class="delete">Delete</button></td>
+                        </tr>
+                        <tr>
+                            <td>December 1st 2021</td>
+                            <td>00:00</td>
+                            <td>December 24th 2021</td>
+                            <td>23:59</td>
+                            <td>Not Available</td>
+                            <td><button class="edit">Edit</button></td>
+                            <td><button class="delete">Delete</button></td>
+                        </tr>
+                        <tr>
+                            <td>November 11th 2021</td>
+                            <td>10:00</td>
+                            <td>November 11th 2021</td>
+                            <td>14:00</td>
+                            <td>Not Available</td>
+                            <td><button class="edit">Edit</button></td>
+                            <td><button class="delete">Delete</button></td>
+                        </tr>
+                    </tbody>
+                </Table>
             </div>
          );
     }
