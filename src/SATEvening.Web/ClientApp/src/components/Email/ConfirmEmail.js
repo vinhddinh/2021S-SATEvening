9265e33b1838db21484a3b1a1a3ff381b499500d
s-li1 <stevenli8892@hotmail.com.au>
2021-11-08 16:23:32 +1100

    edit confirm email message

--- src/SATEvening.Web/ClientApp/src/components/Email/ConfirmEmail.js
+++ src/SATEvening.Web/ClientApp/src/components/Email/ConfirmEmail.js
@@ -9,11 +9,10 @@ export class ConfirmEmail extends Component {
             <div className="bg">
                 <div className="childs">
                     <h1 className="center"> Check Email </h1>
-                    <p className="center"> An email has been sent to: </p>
-                    <p className="center"> I'm not sure how to get the email here </p>
+                    <p className="center"> An email has been sent to your mailbox.</p>
                     <div>
                         <p className="center"> Please check your inbox and spam folder.</p>
-                        <p className="center"> To continue, please follow the instructions in the email </p>
+                        <p className="center"> To continue, please follow the instructions in the email.</p>
                     </div>
                     <div className="center">
                         <Link to="/"><button className="button">Go Back To Login </button></Link>
